39a74e7b4c5acc5a515911ad51c3390f87520879
Alfrex <alfrex.r@gmail.com>
2020-08-20 22:42:24 +0900

    vard added

---
+++ styles/vars.js
@@ -0,0 +1,90 @@
+export const colors = {
+  black: {
+    normal: '#000000'
+  },
+  white: {
+    normal: '#ffffff'
+  },
+  gray: {
+    lighter: '#EBEBEB',
+    light: '#D5D5D5',
+    medium: {
+      normal: '#EEEEEE',
+      10: 'rgba(238, 238, 238, 0.1)',
+      20: 'rgba(238, 238, 238, 0.2)',
+      30: 'rgba(238, 238, 238, 0.3)',
+      75: 'rgba(238, 238, 238, 0.75)'
+    },
+    dark: '#A9AAAE',
+    darker: '#606163'
+  },
+  orange: {
+    light: '#fd9d20',
+    normal: '#ff7d08',
+    dark: '#ba6108'
+  },
+  blue: {
+    light: '#46adff',
+    normal: '#0d89eb',
+    dark: '#076fc1',
+    darker: '#1a3b5b'
+  },
+  red: {
+    light: '#ff5745',
+    normal: '#f92b00',
+    dark: '#b21100'
+  },
+  green: {
+    light: '#32db86',
+    normal: '#00C300',
+    checkbox: '#00B900'
+  },
+  purple: {
+    normal: 'purple'
+  }
+}
+
+export const space = {
+  xxs: '2px',
+  xs: '6px',
+  s: '12px',
+  m: '24px',
+  l: '30px',
+  xl: '36px'
+}
+
+export const fsize = {
+  tiny: '12px',
+  small: '14px',
+  normal: '16px',
+  big: '18px',
+  bigger: '20px',
+  biggest: '65.5px'
+}
+
+export const fweight = {
+  w1: 100,
+  w2: 200,
+  w3: 300,
+  w4: 400,
+  w5: 500,
+  w6: 600,
+  w7: 700,
+  w8: 800,
+  w9: 900
+}
+
+export const LINEH = 1.3
+
+export const zindex = {
+  one: 1,
+  ten: 10,
+  alpha: 100,
+  beta: 500,
+  ganma: 700,
+  overlay: 800,
+  offmenu: 900,
+  offcanvas: 1000,
+  modal: 1100,
+  preloader: 2000
+}
