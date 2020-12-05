1149069a27e755b8f34cfe69596fe42cfb3f2507
Karl Bruder <karl.j.bruder@gmail.com>
2020-12-05 18:30:40 +0900

    ssssssss

--- src/pages/faqPage.tsx
+++ src/pages/faqPage.tsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { graphql, Link } from "gatsby"
 import useTranslations from "../components/useTranslations"
 import FAQPageComponent from "../components/pages/FAQPage/index"
 
+const query = graphql`
+  query Index {
+    rawData: allFile(filter: { sourceInstanceName: { eq: "translations" } }) {
+      edges {
+        node {
+          name
+          translations: childTranslationsJson {
+            hello
+            subline
+            backToHome
+            contact {
+              id
+              type
+              address
+            }
+            copyrightContent
+            emailCTA
+            events {
+              eventID
+              time
+              title
+              date
+              location
+            }
+            social {
+              id
+              service
+              link
+            }
+            socialCTA
+            upcomingEvent {
+              label
+              linkContent
+              buttonContent
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const FAQPage = () => {
-    const { contact, copyrightContent, emailCTA, faqs, socialCTA } = useTranslations()
+    const { contact, copyrightContent, emailCTA, faqs, socialCTA } = useTranslations(query)
     return (
         <div>
             <FAQPageComponent 
