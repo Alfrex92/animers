42f98f354c9a6089972afb0cecd0ff20f8363806
Karl Bruder <karl.j.bruder@gmail.com>
2021-03-04 20:58:42 +0900

    Added custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import image404 from "../images/404.png";
+
+const Animers404Page = () => {
+    return (
+        <div style={{textAlign: "center"}}>
+            <h1>404 Page Not Found ¯\_(ツ)_/¯</h1>
+            <img src={image404} alt="No events because of COVID"/>
+        </div>
+    )
+}
+
+export default Animers404Page;
